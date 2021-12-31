cfed44f5d9003c156981e61fafa6c0de835d384b
KirbyHatena <kuripa1111@gmail.com>
2021-12-31 16:20:20 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-echo __FILE__;
-?>
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php echo __FILE__ ?>
+  </body>
+</html>
